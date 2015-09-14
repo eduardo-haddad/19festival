aa2622dc404eeca97ecbbfb815b974e377644c2b
Eduardo Haddad <eduardo@videobrasil.org.br>
2015-09-14 19:09:28 -0300

    14 de setembro

--- expo_projetos.php
+++ expo_projetos.php
@@ -62,8 +62,7 @@ $titulo = $idioma == 'pt' ? 'Projetos Comissionados' : 'Comissioned Projects';
                         <a href="expo_convidados.php"><?php pten('artistas convidados', 'guest artists') ?></a> | 
                         <a href="expo_projetos.php"><span style="text-decoration: underline"><?php pten('projetos comissionados', 'Comissioned projects') ?></span></a> | 
                         <a href="expo_obras.php"><?php pten('obras selecionadas', 'Selected works') ?></a> | 
-                        <a href="expo_paralela.php"><?php pten('exposição paralela', 'Parallel exhibition') ?></a> |
-                        <a href="expo_locais.php"><?php pten('Locais', 'Venues') ?></a>
+                        <a href="expo_paralela.php"><?php pten('exposição paralela', 'Parallel exhibition') ?></a>
                     </p>      
                 </div>
             </div>
