d41747039a2e04b70dc83e8df059a185b3009ee6
Eduardo Haddad <eduardo@videobrasil.org.br>
2015-11-25 17:17:52 -0200

    mudança de texto oficina lambada

--- elements/pp/oficinas.php
+++ elements/pp/oficinas.php
@@ -86,14 +86,14 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/elements/config.php");
                 //Evento 1 - PT
                 pten('<p>O artista malinês Abdoulaye Konaté conduz uma oficina de monotipia. A partir da prática do ateliê coletivo, o artista aproxima o público de sua poética e de linguagens artísticas tradicionais do Mali, seu local de origem.
                     </p>
-                    <p><span class="bold">Para participar</span> é preciso ter mais de 12 anos. Inscrições gratuitas, no local, a partir do dia 3.10. <span class="bold">Vagas limitadas.</span>
+                    <p><span class="bold">Para participar</span> é preciso ter mais de 11 anos. Inscrições gratuitas, no local, a partir do dia 3.10. <span class="bold">Vagas limitadas.</span>
                     </p>',
 
                 //Evento 1 - EN
                 '<p>The Malian artist Abdoulaye Konaté will lead a workshop on monotypes. Employing a collective studio practice, Konaté will introduce the public to his poetics and to traditional art languages from Mali, his native country.
                 </p>
 
-                <p><span class="bold">How to participate</span> participants must be 12 or older. Registration is free and will be available onsite from Oct 3. <span class="bold">Seats are limited</span>.</p>
+                <p><span class="bold">How to participate</span> participants must be 11 or older. Registration is free and will be available onsite from Oct 3. <span class="bold">Seats are limited</span>.</p>
                 ');  ?> 
                 </div> 
             </div>
@@ -129,14 +129,14 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/elements/config.php");
                 //Evento 2 - PT
                 pten('<p>A partir do trabalho da artista Ting-Ting Cheng, que apresenta uma biblioteca sobre lugares inexistentes na exposição Panoramas do Sul | Projetos comissionados, o público explora livros, histórias e imagens para construir, coletivamente, um repertório conceitual e imagético em torno da ideia de um lugar do Sul. 
                     </p>
-                    <p><span class="bold">Para participar</span> é preciso ter mais de 16 anos e inglês avançado. Inscrições gratuitas, a partir de 3.10, na Convivência Sesc Pompeia, com os mediadores da exposição. <span class="bold">Vagas limitadas.</span>
+                    <p><span class="bold">Para participar</span> é preciso ter mais de 15 anos e inglês avançado. Inscrições gratuitas, a partir de 3.10, na Convivência Sesc Pompeia, com os mediadores da exposição. <span class="bold">Vagas limitadas.</span>
                     </p>',
 
                 //Evento 2 - EN
                 '<p>Through the work of the artist Ting-Ting Cheng, who will present a library of non-existent places in the Southern Panoramas | Commissioned projects exhibition, the public will explore books, stories, and images to build together a conceptual, imagistic repertoire about the place of the South.
                 </p>
 
-                <p><span class="bold">How to participate</span> participants must be 16 or older; advanced English skills required. Registration is free and begins on Oct 3 at Sesc Pompeia’s Convivência area, with the exhibition’s mediators. <span class="bold">Seats are limited</span>.</p>
+                <p><span class="bold">How to participate</span> participants must be 15 or older; advanced English skills required. Registration is free and begins on Oct 3 at Sesc Pompeia’s Convivência area, with the exhibition’s mediators. <span class="bold">Seats are limited</span>.</p>
                 ');  ?> 
                 </div> 
             </div>
@@ -172,14 +172,14 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/elements/config.php");
                 //Evento 3 - PT
                 pten('<p>O artista Carlos Monroy propõe uma vivência que evoca elementos de memória e construção familiar a partir do corpo. A oficina gera reflexões sobre origem, mestiçagem cultural e construção folclórica, e termina em uma prática de dança.  
                     </p>
-                    <p><span class="bold">Para participar</span> é preciso ter mais de 16 anos. Inscrições gratuitas, a partir do dia 3.11, no local ou na Convivência Sesc Pompeia, com os mediadores da exposição <span class="bold">Vagas limitadas.</span>
+                    <p><span class="bold">Para participar</span> é preciso ter mais de 15 anos. Inscrições gratuitas, na Convivência Sesc Pompeia ou no Galpão VB (com os mediadores da exposição), ou pelo email <a href="mailto:portfolio@zebra5.com.br" class="link">portfolio@zebra5.com.br</a>. <span class="bold">Vagas limitadas.</span>
                     </p>',
 
                 //Evento 3 - EN
                 '<p>The artist Carlos Monroy proposes an experience that evokes elements of memory and family construction centered on the body. The workshop elicits reflections about origin, cultural miscegenation, and the building of folklore, and ends with a dance practice. 
                 </p>
 
-                <p><span class="bold">How to participate</span> participants must be 16 or older. Registration is free and will be available from Nov 3, onsite or at Sesc Pompeia’s Convivência area, with the exhibition’s mediators. <span class="bold">Seats are limited</span>.</p>
+                <p><span class="bold">How to participate</span> participants must be 15 or older. Registration is free, at Sesc Pompeia’s Convivência area or at Galpão VB (with the exhibition’s mediators), or by sending an email to <a href="mailto:portfolio@zebra5.com.br" class="link">portfolio@zebra5.com.br</a>. <span class="bold">Seats are limited</span>.</p>
                 ');  ?> 
                 </div> 
             </div>
